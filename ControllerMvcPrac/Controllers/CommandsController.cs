63af0e06a95d54539b8afc011fda175e212f90ae
Michael W <walkerrepair255@yahoo.com>
2022-01-23 12:27:03 -0500

    Add project files.

---
+++ ControllerMvcPrac/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using ControllerMvcPrac.Data;
+using ControllerMvcPrac.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerMvcPrac.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+        //Get api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ ControllerMvcPrac/Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+﻿using ControllerMvcPrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerMvcPrac.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>()
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and Pan" },
+                new Command { Id = 1, HowTo = "Cut Bread", Line = "Get knife", Platform = "Cutting Board" },
+                new Command { Id = 2, HowTo = "Eat Grapes", Line = "Wash Grapes", Platform = "Bowl" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and Pan" };
+        }
+    }
+}
